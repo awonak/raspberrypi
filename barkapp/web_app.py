83e56defa0aa4bf8fc7c4490155b1c2d5b976adc
Adam Wonak <adam.wonak@gmail.com>
2018-01-24 22:21:15 -0600

    client initial commit.

--- barkapp/web_app.py
+++ barkapp/web_app.py
@@ -5,17 +5,30 @@ bark events.
 
 """
 from flask import Flask
+from flask import jsonify
 
-import rpi_app
-
+try:
+    import rpi_app
+    rpi_app.start()
+    alerts = rpi_app.ALERTS
+except RuntimeError as e:
+    import datetime
+    import time
+    alerts = [datetime.datetime.now()]
+    time.sleep(1)
+    alerts.append(datetime.datetime.now())
 
 app = Flask(__name__)
-rpi_app.start()
 
 @app.route('/')
 def index():
     """List the alert timestamps."""
     output = 'Barks: <br/><ul>'
-    for bark in rpi_app.ALERTS:
+    for bark in alerts:
         output += '<li>' + bark.isoformat()
     return output
+
+@app.route('/api/barks')
+def barks():
+    """List the alert timestamps."""
+    return jsonify(alerts)
